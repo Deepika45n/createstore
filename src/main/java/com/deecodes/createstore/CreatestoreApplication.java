dc8ad984febcd92922f4434e0185f88d6a674add
Deepika45n <anjali81399589322gmail.com>
2026-06-27 11:03:17 +0530

    chore:project setup donne

---
+++ src/main/java/com/deecodes/createstore/CreatestoreApplication.java
@@ -0,0 +1,13 @@
+package com.deecodes.createstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatestoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreatestoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deecodes/createstore/CreatestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deecodes.createstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatestoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
